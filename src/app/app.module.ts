c06ad971d88f2278122a1feaf5bc96b7643a2b21
Aman Gupta <67543598+aman71010@users.noreply.github.com>
2023-06-10 16:13:46 +0530

    created attribute directive using renderer2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BasicHighlightDirective } from 'src/basic-highlight/basic-highlight.directive';
+import { BasicHighlightDirective } from 'src/app/basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BasicHighlightDirective
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule
